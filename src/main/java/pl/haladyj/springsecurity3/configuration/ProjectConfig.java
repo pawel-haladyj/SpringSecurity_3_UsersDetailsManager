32b7af7d60d600318216a5fb8565606eb5f88f0b
Pawel Haladyj <pawel@haladyj.pl>
2020-04-17 22:12:08 +0200

    summary

--- src/main/java/pl/haladyj/springsecurity3/configuration/ProjectConfig.java
+++ src/main/java/pl/haladyj/springsecurity3/configuration/ProjectConfig.java
@@ -1,15 +1,11 @@
 package pl.haladyj.springsecurity3.configuration;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
